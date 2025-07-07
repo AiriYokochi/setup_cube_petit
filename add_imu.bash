2459742649544699df873df4800958cc692af4c5
AYokochi <airi.yokochi@g.softbank.co.jp>
2025-07-07 16:25:40 +0900

    add imu dependency

--- add_imu.bash
+++ add_imu.bash
@@ -9,5 +9,7 @@ sudo udevadm trigger
 # ROS build
 cd ~/ros/src/
 git clone -b ros2 --recursive https://github.com/ElettraSciComp/witmotion_IMU_ros.git witmotion_ros
+sudo apt install -y libqt5serialport5-dev
+cd ~/ros
 colcon build --symlink-install --packages-select witmotion_ros
 source install/setup.bash
