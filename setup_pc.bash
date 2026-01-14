bbe4273f3610caf8230a6201a65229dea41fafb9
AYokochi <airi.yokochi@g.softbank.co.jp>
2026-01-14 10:18:51 +0900

    update setup pc

--- setup_pc.bash
+++ setup_pc.bash
@@ -1,25 +1,43 @@
 #!/bin/bash
 
+
+# set home dir in English
+LANG=C xdg-user-dirs-gtk-update
+
+# Apt upgrade
+echo -e '\e[1;31m == apt update / upgrade == \e[m'
+sudo apt update
+sudo apt upgrade -y
+
+echo -e '\e[1;31m == apt install == \e[m'
+# Japanese Input
+sudo apt install -y ibus-mozc wget gpg
+# Chrome for face
+wget -qO- https://dl.google.com/linux/linux_signing_key.pub \
+  | gpg --dearmor \
+  | sudo tee /usr/share/keyrings/google-chrome.gpg > /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/google-chrome.gpg] http://dl.google.com/linux/chrome/deb/ stable main" \
+  | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt update
+sudo apt install -y google-chrome-stable
+# remove keyring for open Chrome
+rm ~/.local/share/keyrings/login.keyring
+
 # Udevs
 cd ~/work/setup_cube_petit
-echo -e '\e[1;31m == Install and setup Udev rules == \e[m'
+echo -e '\e[1;31m == Setup Udev rules == \e[m'
 sudo cp udevs/* /etc/udev/rules.d/
 sudo cp -r shell_scripts ~/
 echo "alias psgrepkill=\"bash ~/shell_scripts/ps_grep_kill.sh \$@\"" >> ~/.bashrc
 cd
 
-# set home dir in English
-LANG=C xdg-user-dirs-gtk-update
+echo -e '\e[1;31m == Setup Udev rules == \e[m'
 
-# blanc screen iff
+# blanc screen off
 gsettings set org.gnome.desktop.session idle-delay  0
 
 # hide side-bar
 gsettings set org.gnome.shell.extensions.dash-to-dock autohide false && gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false && gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
-
-# Download Chrome
-cd ~/Downloads
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 # Set Favorite app
 gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.Terminal.desktop', 'code.desktop']"
@@ -27,17 +45,14 @@ gsettings set org.gnome.shell favorite-apps "['google-chrome.desktop', 'org.gnom
 # Hide Home fron desktop
 gsettings set org.gnome.shell.extensions.ding show-home false
 
-# Japanese Input
-sudo apt install -y ibus-mozc
-
 # Wall paper
 mkdir -p ~/Pictures/Wallpapers
 cp ~/work/setup_cube_petit/pictures/*.png ~/Pictures/Wallpapers
 gsettings set org.gnome.desktop.background picture-uri "file://$HOME/Pictures/Wallpapers/happy.png"
 
-# remove keyring for open Chrome
-rm ~/.local/share/keyrings/login.keyring
-
 # Auto login
 sudo sed -i '/^\[daemon\]/a AutomaticLoginEnable=true\nAutomaticLogin='$(whoami) /etc/gdm3/custom.conf
+
+echo -e '\e[1;31m == Setup PC Finished == \e[m'
+
 sudo systemctl restart gdm
