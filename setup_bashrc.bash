8a07a152ea6ce104f37b5311e0de859f0aecf12c
AiriYokochi <airi.yokochi@g.softbank.co.jp>
2026-07-24 12:44:26 +0900

    bashrc: add start_cube_petit_service alias
    Companion to stop_cube_petit_service.

--- setup_bashrc.bash
+++ setup_bashrc.bash
@@ -76,6 +76,7 @@ ${OPENAI_LINE}
 [ -f "\$HOME/.config/cube_petit/env" ] && source "\$HOME/.config/cube_petit/env"
 
 alias stop_cube_petit_service="systemctl --user stop cube-petit-bringup.service"
+alias start_cube_petit_service="systemctl --user start cube-petit-bringup.service"
 alias 01_BRING="ros2 launch cube_petit_bringup cube_petit_bringup.launch.py${BRING_FACE_COLOR_ARG}"
 alias 02_DEMO="ros2 launch cube_petit_scenario cube_petit_talk_demo.launch.py"
 alias 04_TALK_START="ros2 service call /${ROBOT_NAMESPACE}/enable_realtime_conversation std_srvs/srv/SetBool \"data: true\""
